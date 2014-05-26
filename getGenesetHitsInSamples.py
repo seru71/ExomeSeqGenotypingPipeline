078637db86df90b272f4d299c941be79d5032836
seru71 <pawel.sztromwasser@gmail.com>
2014-05-26 11:10:26 +0200

    debugged getGenesetHitsInSamples.py

--- getGenesetHitsInSamples.py
+++ getGenesetHitsInSamples.py
@@ -4,7 +4,7 @@ For a list of genes in GENE_SET_FILE and a list of files with variant calls
 (one sample per file; currently annovar avinput.refGene.variant_function files) 
 print out which genes from the gene set are found altered (per sample)
 
-author: Paweł Sztromwasser (pawel.sztromwasser@k2.uib.no  
+author: Pawel Sztromwasser (pawel.sztromwasser@k2.uib.no  
 """
 
 def getGenes(gene_set_file):
@@ -15,7 +15,7 @@ def getGenes(gene_set_file):
 	return set(genes)
 
 
-def parenthesis_aware_split(string, delim=',', open_par='(', close_par=')'):
+def parenthesisAwareSplit(string, delim=',', open_par='(', close_par=')'):
 	"""	Split outside of parenthesis (i.e. ignore delimiters within parenthesis."""
 	out = []
 	s = ''
@@ -33,7 +33,7 @@ def parenthesis_aware_split(string, delim=',', open_par='(', close_par=')'):
 	return out + [s]
 
 
-def findGenesInSamples(sample_files, geneset, gene_column = 1, delim='\t'):
+def findGenesetHitsInSamples(sample_files, geneset, gene_column = 1, delim='\t'):
 	"""
 	Iterate over the per-sample variant files and look for genes from the geneset.
 	Returns a dict in form {sample: [gene1,gene2]}, where gene1 and gene2 belong to the geneset.
@@ -43,6 +43,7 @@ def findGenesInSamples(sample_files, geneset, gene_column = 1, delim='\t'):
 	map={f:[] for f in sample_files}
 	
 	for fname in sample_files:
+		sys.stderr.write('Processing '+ fname + '...')
 		f = open(fname)
 		for l in f.xreadlines():
 			gene_entry = l.split('\t')[gene_column]
@@ -50,8 +51,8 @@ def findGenesInSamples(sample_files, geneset, gene_column = 1, delim='\t'):
 			gene_entry = gene_entry.strip().strip('"')		# clean the gene name
 			genes=[gene_entry]
 			if gene_entry.find(',') >= 0: 
-				genes = parenthesis_aware_split(gene_entry,delim=',')	# split multi gene entries
-			genes = [parenthesis_aware_split(gene,delim=';') for gene in genes]
+				genes = parenthesisAwareSplit(gene_entry,delim=',')	# split multi gene entries
+			genes = [parenthesisAwareSplit(gene,delim=';') for gene in genes]
 			genes = set([gene for sublist in genes for gene in sublist])  # get unique gene ids only
 			
 			for gene in genes:
@@ -62,6 +63,8 @@ def findGenesInSamples(sample_files, geneset, gene_column = 1, delim='\t'):
 					map[fname] += [gene]
 										
 		f.close()
+                sys.stderr.write('done\n')
+
 		
 	return map
 
@@ -72,17 +75,19 @@ import glob
 
 if __name__ == '__main__':
 	
-	if len(sys.argv) < 2:
-		print 'Usage: getGenesetHitsInSample.py AVINPUT_FILE(S) GENE_SET_FILE'
-		print 'Example: getGenesetHitsInSample.py sample_1234.avinput.refGene.variant_function interesting_genes.txt'
-		print 'Example: getGenesetHitsInSample.py annovar-files/*.avinput.refGene.variant_function interesting_genes.txt'
+	if len(sys.argv) < 3:
+		print 'Usage:', sys.argv[0], 'AVINPUT_FILE(S) GENE_SET_FILE'
+		print 'Example:', sys.argv[0],'sample_1234.avinput.refGene.variant_function interesting_genes.txt'
+		print 'Example:', sys.argv[0],'"annovar-files/*.avinput.refGene.variant_function" interesting_genes.txt'
+		sys.exit(0)
 	
-	avinput_files = glob.glob(sys.argv[0])
-	geneset = getGenes(sys.argv[1])
+	variant_files = glob.glob(sys.argv[1])
+	geneset = getGenes(sys.argv[2])
 	
-	map = mapGenesInSamples(avinput_files, geneset, gene_column=1)
-	
-	for sample in map:
+	map = findGenesetHitsInSamples(variant_files, geneset, gene_column=1)
+
+#	print '---------------'	
+	for sample in sorted(map.keys()):
 		print sample, ':', ','.join(map[sample])
 	
 	
