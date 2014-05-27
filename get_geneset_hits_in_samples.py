c3471bb13e4d191bacbb78e97409f960e4d85a3f
seru71 <pawel.sztromwasser@gmail.com>
2014-05-27 15:36:18 +0200

    changed get_geneset_hits_in_samples.py to spit out detailed variant information

--- get_geneset_hits_in_samples.py
+++ get_geneset_hits_in_samples.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 For a list of genes in GENE_SET_FILE and a list of files with variant calls 
-(one sample per file; currently annovar avinput.refGene.variant_function files) 
+(one sample per file; currently annovar avinput.hg19_multianno.with_omim.csv files) 
 print out which genes from the gene set are found altered (per sample)
 
 author: Pawel Sztromwasser (pawel.sztromwasser at k2.uib.no)  
@@ -16,23 +16,27 @@ def get_genes(gene_set_file):
 
 from utility_functions import parenthesis_aware_split
 
-def find_geneset_hits_in_samples(sample_files, geneset, gene_column = 1, delim='\t'):
+def find_geneset_hits_in_samples(sample_files, geneset, gene_column_name = 'Gene.refGene'):
 	"""
-	Iterate over the per-sample variant files and look for genes from the geneset.
-	Returns a dict in form {sample: [gene1,gene2]}, where gene1 and gene2 belong to the geneset.
-	The same gene appearing twice in the sample's list indicates two distinct variants in the gene.
+	Iterate over the per-sample variant tables (csv) and look for genes from the geneset.
+	Returns a dict in form {sample: {gene1:[variant_line], gene2:[variant_line1, variant_line2]}}, where gene1 and gene2 belong to the geneset, 
+	and variant_line is the entire line from csv file.
+	As the same gene can appear twice with distinct variants, the dict[sample][gene] is a list.
 	"""
 	
-	map={f:[] for f in sample_files}
+	map={s:{} for s in sample_files}
 	
 	for fname in sample_files:
 		sys.stderr.write('Processing '+ fname + '...')
 		f = open(fname)
+		
+		header = quote_aware_split(table_in.readline().strip())
+		gene_col_index = header.index(gene_column_name)
+		
 		for l in f.xreadlines():
-			gene_entry = l.split('\t')[gene_column]
-			
-			gene_entry = gene_entry.strip().strip('"')		# clean the gene name
-			genes=[gene_entry]
+			gene_entry = quote_aware_split(l)[gene_col_index]
+			gene_entry = gene_entry.strip().strip('"') 	# clean the gene name		
+			genes=[gene_entry]						
 			if gene_entry.find(',') >= 0: 
 				genes = parenthesis_aware_split(gene_entry,delim=',')	# split multi gene entries
 			genes = [parenthesis_aware_split(gene,delim=';') for gene in genes]
@@ -43,11 +47,13 @@ def find_geneset_hits_in_samples(sample_files, geneset, gene_column = 1, delim='
 					gene = gene[:gene.find('(')] # strip the transcript change in parenthesis
 			
 				if gene in geneset:
-					map[fname] += [gene]
+					try:
+						map[fname][gene] += [l]
+					except KeyError:
+						map[fname][gene] = [l]
 										
 		f.close()
-                sys.stderr.write('done\n')
-
+        sys.stderr.write('done\n')
 		
 	return map
 
@@ -59,9 +65,9 @@ import glob
 if __name__ == '__main__':
 	
 	if len(sys.argv) < 3:
-		print 'Usage:', sys.argv[0], 'AVINPUT_FILE(S) GENE_SET_FILE'
-		print 'Example:', sys.argv[0],'sample_1234.avinput.refGene.variant_function interesting_genes.txt'
-		print 'Example:', sys.argv[0],'"annovar-files/*.avinput.refGene.variant_function" interesting_genes.txt'
+		print 'Usage:', sys.argv[0], 'CSV_FILE(S) GENE_SET_FILE'
+		print 'Example:', sys.argv[0],'sample_1234.avinput.hg19_multianno.with_omim.csv interesting_genes.txt'
+		print 'Example:', sys.argv[0],'"annovar-files/*.avinput.hg19_multianno.with_omim.csv" interesting_genes.txt'
 		sys.exit(0)
 	
 	variant_files = glob.glob(sys.argv[1])
@@ -71,6 +77,10 @@ if __name__ == '__main__':
 
 #	print '---------------'	
 	for sample in sorted(map.keys()):
-		print sample, ':', ','.join(map[sample])
+		print sample
+		for gene in sorted(map[sample].keys()):
+			print ''.join(map[sample][gene])
+		print '------------------'
+		
 	
 	
