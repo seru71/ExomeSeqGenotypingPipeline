3e504e0c19d69acf58415baefe1f34ed03b59f21
seru71 <pawel.sztromwasser@gmail.com>
2014-09-23 14:41:05 +0200

    added chr to chromosome names

--- pipeline_annovar.py
+++ pipeline_annovar.py
@@ -563,9 +563,9 @@ def count_hetz_and_homz_per_chr(infiles, table_files):
 
     # the variant calls are expected to appear sorted by chromosome name in the following order
     # any other order should trigger an exception
-    chromosomes = ['1','2','3','4','5','6','7','8','9',
+    chromosomes = ['chr'+c for c in ['1','2','3','4','5','6','7','8','9',
                    '10','11','12','13','14','15','16','17','18','19',
-                   '20','21','22','X','Y','GL000209.1']
+                   '20','21','22','X','Y']]
 
     hetz = open(table_files[0], 'w')
     homz = open(table_files[1], 'w')
